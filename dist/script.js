b77671845d5d743947b5242ec4827be362f58bf4
NikDoom <49077681+NikDoom@users.noreply.github.com>
2020-11-16 22:49:55 +0300

    k

--- dist/script.js
+++ dist/script.js
@@ -1,15 +1,22 @@
 let catalog = document.getElementById("catalog");
+let uniq = document.getElementById("uniq");
+let about = document.getElementById("about");
+let contacts = document.getElementById("contacts");
+
 
 catalog.onclick = function(event) {
 	catalog.classList.add("makeScale");
 	function moveToNext() {
 		document.location.href = "catalog/catalog.html";
-	};
-	setTimeout(moveToNext, 700);
-	function removeScaling() {
 		catalog.classList.remove("makeScale");
 	};
-	setTimeout(removeScaling, 1500);
+	setTimeout(moveToNext, 700);
 };
 
-// setTimeout(scaling, 2000);
+uniq.onclick = function(event) {
+	uniq.classList.add("makeScale");
+	function moveToNext() {
+		document.location.href = "uniq/uniq.html";
+		uniq.classList.remove("makeScale");
+	};
+};
