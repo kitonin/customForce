d8b420b6d1de0c65e3ac61a0db792de38b0fe873
NikDoom <49077681+NikDoom@users.noreply.github.com>
2020-11-16 23:13:30 +0300

    full1.4

--- dist/script.js
+++ dist/script.js
@@ -17,7 +17,7 @@ uniq.onclick = function(event) {
 	uniq.classList.add("makeScale");
 	function moveToNext() {
 		document.location.href = "uniq/uniq.html";
-		// uniq.classList.remove("makeScale");
+		uniq.classList.remove("makeScale");
 	};
 	setTimeout(moveToNext, 700);
 };
