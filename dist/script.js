ae882ae124777a558fafef73c91fa8408b17f371
NikDoom <49077681+NikDoom@users.noreply.github.com>
2020-12-16 16:13:48 +0300

    ;lmkmkmkm

--- dist/script.js
+++ dist/script.js
@@ -8,7 +8,7 @@ catalog.onclick = function(event) {
 	catalog.classList.add("makeScale");
 	function moveToNext() {
 		document.location.href = "catalog/catalog.html";
-		catalog.classList.remove("makeScale");
+		// catalog.classList.remove("makeScale");
 	};
 	setTimeout(moveToNext, 500);
 };
@@ -17,7 +17,7 @@ uniq.onclick = function(event) {
 	uniq.classList.add("makeScale");
 	function moveToNext() {
 		document.location.href = "uniq/uniq.html";
-		uniq.classList.remove("makeScale");
+		// uniq.classList.remove("makeScale");
 	};
 	setTimeout(moveToNext, 500);
 };
@@ -26,7 +26,7 @@ about.onclick = function(event) {
 	about.classList.add("makeScale");
 	function moveToNext() {
 		document.location.href = "about/about.html";
-		about.classList.remove("makeScale");
+		// about.classList.remove("makeScale");
 	};
 	setTimeout(moveToNext, 500);
 };
@@ -35,7 +35,7 @@ contacts.onclick = function(event) {
 	contacts.classList.add("makeScale");
 	function moveToNext() {
 		document.location.href = "contacts/contacts.html";
-		contacts.classList.remove("makeScale");
+		// contacts.classList.remove("makeScale");
 	};
 	setTimeout(moveToNext, 500);
 };
@@ -46,7 +46,7 @@ onePageScroll(".main", {
    sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
    easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", 
                                     // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-   animationTime: 666,             // AnimationTime let you define how long each section takes to animate
+   animationTime: 444,             // AnimationTime let you define how long each section takes to animate
    pagination: false,                // You can either show or hide the pagination. Toggle true for show, false for hide.
    updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
    // beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
