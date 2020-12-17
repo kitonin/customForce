c0be98043330fbe833ad332417b29aca82d4fa49
NikDoom <49077681+NikDoom@users.noreply.github.com>
2020-12-17 03:30:18 +0300

    head

--- dist/popup.js
+++ dist/popup.js
@@ -1,21 +1,22 @@
 let button = document.getElementById('hamburger-icon');
 let popUp = document.getElementById('popup-menu');
-let body = document.getElementById('body');
-console.log(button);
-console.log(popUp);
+let header = document.getElementById('header');
+
 button.onclick = function() {
 	if (popUp.style.display == 'none') {	
 		popUp.style.display = 'block';
 		button.classList.add('active');
-		body.style.overflow = 'hidden';
+		header.style.position = 'fixed';
+		// header.style.top = '0';
 	}
 	else {
 		button.classList.remove('active');
 		popUp.classList.add('menu_out');
+		header.style.position = '';
+		// header.style.top = ';';
 		setTimeout(function () {
 			popUp.classList.remove('menu_out');
 			popUp.style.display = 'none';
-			body.style.overflow = '';
 		},1000)
 	}
 }
